106bd1fbd86102f9a675e911c2cb8267389504ef
Alberto Alexandru Cieri <alberto.cieri@info.uaic.ro>
2016-06-01 11:33:04 +0300

    improved filtering for rest api, fixed login bug

---
+++ api/databaseFunctions/personFilter.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alber_000 on 6/1/2016.
+ */
+var filter=require('./genericFilter');
+
+function personFilter(req,res)
+{
+    //to be implemented 
+}
+
+module.exports=personFilter;
--- routes/Datafiltering.js
+++ routes/Datafiltering.js
@@ -1,9 +1,21 @@
 
 var express = require('express');
 var router = express.Router();
+var solvedTasksFilter=require('../api/databaseFunctions/solvedTasksFilter');
+var personFilter=require('../api/databaseFunctions/personFilter');
 
 router.get('/', function(req, res, next) {
     res.render('Datafiltering', { title: 'Express' });
 });
+
+router.get('/solvedTasksFilter',function(req,res) {
+    res.render('solvedTasksFiltering');
+});
+router.post('/solvedTasksFilter',solvedTasksFilter);
+
+router.get('/personFilter',function(req,res) {
+    res.render('personFilter');
+});
+//router.post('/personFilter',personFilter);
 
 module.exports = router;

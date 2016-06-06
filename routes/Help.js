38e5d20c4b07ae35f312a1fb5d082131d8ed4d81
Dan Alexandru <dan.alexandru@info.uaic.ro>
2016-06-06 12:25:14 +0300

    Added grid/list display for json, swagger route attempt, custom scrollbar

--- routes/Help.js
+++ routes/Help.js
@@ -7,8 +7,40 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('Help');
 });
+
+
+/*
+var app = require('../app');
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
+var fs = require('fs');
+
+var options = {
+  swaggerUi: '/swagger.json',
+  controllers: './controllers',
+  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+};
+
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync('api/documentation/api.yaml', 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);*/
+
 router.get('/rest', function(req, res, next) {
+	/*// Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+	app.use(swagger.metadata(swaggerDoc));
+
+	// Validate Swagger requests
+	app.use(swagger.validator(swaggerDoc));
+
+	// Route validated requests to appropriate controller
+	app.use(swagger.router(swaggerDoc, options));
+
+	// Serve the Swagger documents and Swagger UI
+	app.use(swagger.ui(swaggerDoc));*/
+	
     res.render('Help-REST');
 });
+
 
 module.exports = router;

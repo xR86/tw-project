227b5dc0518596621e91a17977153aa92840c03c
xR86 <9412441+xR86@users.noreply.github.com>
2016-06-07 05:43:18 +0300

    updated dashboard - badges, procedures, static d3

--- routes/Dashboard.js
+++ routes/Dashboard.js
@@ -11,7 +11,7 @@ var SimpleOracleDB = require('simple-oracledb');
 var generic=require('../api/databaseFunctions/genericSelect');
  
 SimpleOracleDB.extend(oracledb);
-
+ 
 
 router.get('/',function(req, res, next) {
 	"use strict";
@@ -29,21 +29,34 @@ router.get('/',function(req, res, next) {
          }
  
 		  connection.execute(
-		  "BEGIN :ret := personsCount(); END;",
-		  { ret: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 } },
+		  "BEGIN :ret := personsCount(); :ret2 := solvedTasksCount(); :ret3 := tasksCount(); :ret4 := siteUsersCount(); END;",
+		  { ret: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 },
+		    ret2: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 },
+		    ret3: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 },
+		    ret4: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 } },
 		  function (err, result)
 		  {
 		    if (err) { console.error(err.message); return; }
 		    else{
-		    	console.log(result.outBinds);
+		    	//console.log(result.outBinds);
                 var jsonData = JSON.stringify(result.outBinds);
                 	
                 var parsedData = JSON.parse(jsonData);
                 console.log(parsedData);
                 var fccUserNumber = parsedData.ret;
-                console.log(fccUserNumber);
+                var solvedTasksNumber = parsedData.ret2;
+                var tasksNumber = parsedData.ret3;
+                var siteUsersNumber = parsedData.ret4;
+                //console.log(fccUserNumber);
+                //console.log(solvedTasksNumber);
 
-                res.render('Dashboard', {resultSet:fccUserNumber});
+
+                res.render('Dashboard', {
+                	resultSetUsers:fccUserNumber, 
+                	resultSetSolvedTasks:solvedTasksNumber,
+                	resultSetTasks: tasksNumber,
+                	resultSetSiteUsers: siteUsersNumber
+                });
 		    }
 		    
 		  });
